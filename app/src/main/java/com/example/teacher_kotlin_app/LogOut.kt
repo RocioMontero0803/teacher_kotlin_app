8dc212bf6b2d598c0a85c2572f793a3e44e27371
RocioMontero0803 <Rocio.montero0803@gmail.com>
2023-07-25 09:23:54 -0500

    Made basic structure with user

---
+++ app/src/main/java/com/example/teacher_kotlin_app/LogOut.kt
@@ -0,0 +1,33 @@
+package com.example.teacher_kotlin_app
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.teacher_kotlin_app.databinding.ActivityLogoutBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LogOut : AppCompatActivity() {
+    private lateinit var binding: ActivityLogoutBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    // private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLogoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnSignOutH.setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+
+        }
+        finish()
+
+    }
+}
