b4af4f3a2380213936be0608da8ae0b63415ca22
GaelRR1 <gaelramosrr@gmail.com>
2023-07-26 14:22:33 -0500

    Profile Integrated into the slideshow

--- app/src/main/java/com/example/teacher_kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/teacher_kotlin_app/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.teacher_kotlin_app
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,12 +14,27 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.teacher_kotlin_app.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+    private lateinit var myTextView6: TextView
+    private lateinit var myTextView7: TextView
 
+    companion object {
+        var name: String = ""
+        var email: String = ""
+        var uid: String = ""
+        var role: String = ""
+
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,6 +42,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().reference
+
+
+        val user = mAuth.currentUser
+
+        mDbRef.child("user").child(uid).child("name").get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                name = (task.result?.value as? String).toString()
+            }
+        }
+
+        email = user?.email ?: ""
+        uid = user?.uid ?:""
+        mDbRef.child("user").child(uid).child("role").get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                role = (task.result?.value as? String).toString()
+            }
+        }
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -54,4 +91,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
 }
--- app/src/main/java/com/example/teacher_kotlin_app/ui/profile/Profile.kt
+++ app/src/main/java/com/example/teacher_kotlin_app/ui/profile/Profile.kt
@@ -1,15 +1,34 @@
 package com.example.teacher_kotlin_app.ui.profile
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.example.teacher_kotlin_app.MainActivity.Companion.email
+import com.example.teacher_kotlin_app.MainActivity.Companion.name
+import com.example.teacher_kotlin_app.MainActivity.Companion.role
+import com.example.teacher_kotlin_app.MainActivity.Companion.uid
 import com.example.teacher_kotlin_app.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.Locale
 
 class Profile : Fragment() {
 
+    private lateinit var myTextView: TextView
+    private lateinit var myTextView1: TextView
+    private lateinit var myTextView2: TextView
+    private lateinit var myTextView3: TextView
+    private lateinit var myTextView4: TextView
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+    private lateinit var addProfile: Button
     companion object {
         fun newInstance() = Profile()
     }
@@ -20,13 +39,48 @@ class Profile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+
+        //Change name on logo
+
+        val firstLetter = name.capitalize(Locale.ROOT) ?: ""
+
+
+        //Change name to actual
+
+        myTextView4 = view.findViewById(R.id.nameProfile)
+        myTextView4.text = "Name: Hello $name."
+
+
+
+        //Change email to actual
+        myTextView1 = view.findViewById(R.id.emailProfile)
+        myTextView1.text = "Email: Your email is \"$email\"."
+
+
+
+
+        // Change role to Actual
+        myTextView3 = view.findViewById(R.id.roleProfile)
+        myTextView3.text = "Role: Your role is \"$role\" ."
+
+        myTextView2 = view.findViewById(R.id.uidProfile)
+        myTextView2.text = "Uid: Your uid is \"$uid\"."
+
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().reference
+
+
+
     }
 
 }
